a3e7e8bc4c71421c587bfa71e411cccafc521079
ianbautista <ianfbautista@gmail.com>
2020-07-07 16:38:50 -0700

    added load, mouseout, mouseleave, mousedown, mouseup, click, dblclick, resize

--- js/index.js
+++ js/index.js
@@ -15,11 +15,58 @@ navLinks.forEach((link) => {
 });
 
 // shows a message upon press of any key, used event keydown
-
 window.addEventListener("keydown", () => {
 	alert(`Ooops!!! Stop pressing that!`);
 });
 
-// wheel
+// alerts a message when load finishes using load.
+window.addEventListener("load", () => {
+	alert(`Welcome to Fun Bus!`);
+});
 
-// window.addEventListener("wheel");
+// flips the photo with mouseover. toggle the flip with mouseleave.
+const funBus = document.querySelector("header.intro img");
+funBus.addEventListener("mouseover", (evt) => {
+	evt.target.style.transform = "scaleX(-1)";
+});
+funBus.addEventListener("mouseleave", (evt) => {
+	evt.target.style.transform = null;
+});
+
+// inverts the color of the photo with mousedown. toggles with mouseup.
+funBus.addEventListener("mousedown", (evt) => {
+	evt.target.style.filter = "invert(1)";
+});
+funBus.addEventListener("mouseup", (evt) => {
+	evt.target.style.filter = null;
+});
+
+// change header color when clicked
+const header = document.querySelector("header.main-navigation");
+header.addEventListener("click", () => {
+	header.style.background = "#34C1DB";
+});
+header.addEventListener("mouseleave", () => {
+	header.style.background = null;
+});
+
+// alert message when resized
+window.addEventListener("resize", () => {
+	alert(`Stop resizing me!`);
+});
+
+// preventing nav-items from refreshing page
+navLinks.forEach((link) => {
+	link.addEventListener("click", (evt) => {
+		evt.preventDefault();
+	});
+});
+
+// change paragraph text to uppercase on doubleclick
+const p = document.querySelectorAll("p");
+console.log(p);
+p.forEach((para) => {
+	para.addEventListener("dblclick", (evt) => {
+		evt.target.style.textTransform = "uppercase";
+	});
+});
